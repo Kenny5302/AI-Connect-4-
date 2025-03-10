27ef8ae70458b67744b2498761cb5100e3634284
anorak-lightman <164287745+anorak-lightman@users.noreply.github.com>
2025-03-10 13:32:33 -0700

    Made it playable
    Made it so you give it a board state and it will tell you the best move for the model to make and give you the updated board state. In case of more than one best move it randomly selects one, and in case of no best move it finds a move that will tie. In case of no tie moves it then randomly selects a move to take.

--- connect4.py
+++ connect4.py
@@ -7,6 +7,7 @@ import torch.optim as optim
 from torch.utils.data import Dataset
 from torchvision import datasets, transforms
 import numpy as np
+import random
 
 def resource_path(relative_path):
     base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
@@ -35,21 +36,110 @@ class ConnectFourDeepNet(nn.Module):
         x = F.log_softmax(x, dim = 0)
         return x
 
-model_path = resource_path("model.pth")
 
-model = torch.load(model_path)
+def convert_to_board_state(col1, col2, col3, col4, col5, col6, col7):
+    board_state = []
+    for i in range(6):
+        board_state.append(col1[i])
+        board_state.append(col2[i])
+        board_state.append(col3[i])
+        board_state.append(col4[i])
+        board_state.append(col5[i])
+        board_state.append(col6[i])
+        board_state.append(col7[i])
+    return board_state
 
-model.eval()
+def find_best_move(board_state):
 
-with torch.no_grad():
-    input = [1., 1., 1., 2., 2., 1., 0.,
-             2., 2., 2., 1., 2., 2., 0.,
-             2., 1., 1., 2., 1., 1., 0.,
-             1., 2., 1., 2., 2., 2., 2.,
-             1., 1., 2., 2., 1., 1., 1.,
-             2., 1., 2., 1., 2., 1., 2.,]
-    input = torch.tensor(input)
-    output = model(input)
-    output = output.argmax(0)
-    output = int(output.numpy())
-    print(output)
+    model_path = resource_path("model.pth")
+
+    model = torch.load(model_path)
+
+    model.eval()
+
+    with torch.no_grad():
+        col1 = []
+        col2 = []
+        col3 = []
+        col4 = []
+        col5 = [] 
+        col6 = []
+        col7 = []  
+        i = 1
+        for entry in board_state:
+            if i == 1:
+                col1.append(entry)
+                i += 1
+            elif i == 2:
+                col2.append(entry)
+                i += 1
+            elif i == 3:
+                col3.append(entry)
+                i += 1
+            elif i == 4:
+                col4.append(entry)
+                i += 1
+            elif i == 5:
+                col5.append(entry)
+                i += 1
+            elif i == 6:
+                col6.append(entry)
+                i += 1
+            elif i == 7:
+                col7.append(entry)
+                i = 1
+        column_array = [col1, col2, col3, col4, col5, col6, col7]
+        index = -1
+        attemped_board_states = []
+        winning_board_states = []
+        tie_board_states = []
+        board_state_to_return = []
+        for col in column_array:
+            for i, entry in enumerate(col):
+                if entry == 0: 
+                    if col[i + 1] != 0:
+                        index = i
+                        break
+            if index != -1:
+                col[index] = 2.0
+                new_board_state = convert_to_board_state(col1, col2, col3, col4, col5, col6, col7)
+                attemped_board_states.append(new_board_state)
+                new_board_state_tensor = torch.tensor(new_board_state)
+                winner = model(new_board_state_tensor)
+                winner = winner.argmax(0)
+                winner = int(winner.numpy())
+                if winner == 2:
+                    winning_board_states.append(new_board_state)
+                elif winner == 0:
+                    tie_board_states.append(new_board_state)
+                index = -1
+        if not winning_board_states:
+            if not tie_board_states:
+                random_state = random.randrange(1, 8)
+                board_state_to_return = attemped_board_states[random_state]
+            else: 
+                num_tie_boards = len(tie_board_states)
+                if num_tie_boards == 1:
+                    board_state_to_return = tie_board_states[0]
+                else:
+                    random_state = random.randrange(1, num_tie_boards)
+                    board_state_to_return = tie_board_states[random_state]
+        else:
+            num_winning_boards = len(winning_board_states)
+            if num_winning_boards == 1:
+                board_state_to_return = winning_board_states[0]
+            else:
+                random_state = random.randrange(1, num_winning_boards)
+                board_state_to_return = winning_board_states[random_state]
+
+        return board_state_to_return
+
+
+input = [1., 1., 1., 2., 2., 1., 0.,
+        2., 2., 2., 1., 2., 2., 0.,
+        2., 1., 1., 2., 1., 1., 0.,
+        1., 2., 1., 2., 2., 2., 0.,
+        1., 1., 2., 2., 1., 1., 1.,
+        2., 1., 2., 1., 2., 1., 2.,]
+board_state = find_best_move(input)
+print(board_state)
